f3f9beb4a10e5f453a0365d3cb19d83e92c523e9
Greg Powell <gregtpowell@gmail.com>
2020-12-15 15:31:08 -0600

    new webpack build

--- docs/index_bundle.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable strict */
-$((function(){Bookmarks.bindEventListeners(),API.getBookmarks((o=>{o.forEach((o=>Store.addBookmark(o))),Bookmarks.render();}));}));
